b9b37ca405a0ad4f465c20ab6b6e72ea730a2abb
yoochan han <159185007+yoochan-han@users.noreply.github.com>
2024-05-27 20:35:30 +0000

    #

--- js/script.js
+++ js/script.js
@@ -5,10 +5,12 @@
 // This file contains the JS functions for index.html
 
 function CalculatePyramid() {
+  //get elements by ID
   const length = parseFloat(document.getElementById("get_length").value)
   const width = parseFloat(document.getElementById("get_width").value)
   const height = parseFloat(document.getElementById("get_height").value)
   // caculate
+
   let ans = (length * width * height) / 3
   document.getElementById("Answer").innerHTML = "volume of pyramid is " + ans + "cm³."
 }
